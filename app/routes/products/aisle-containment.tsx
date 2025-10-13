1c4bd2e2166c704190591677f587a29f21a6066d
alabo-excel <iamalaboexcel@gmail.com>
2025-10-13 11:11:21 +0100

    aisle containment and security cages pages commit

--- app/routes/products/aisle-containment.tsx
+++ app/routes/products/aisle-containment.tsx
@@ -11,9 +11,63 @@ export function meta({ }: Route.MetaArgs) {
 };
 
 const AisleContainment = () => {
+    const data = [
+        {
+            title: "Cold Aisle Containment",
+            image: "/images/aisle-containment/dataracks-cold-aisle-containment.jpg"
+        },
+        {
+            title: "Hot Aisle Containment",
+            image: "/images/aisle-containment/dataracks-hot-aisle-containment.jpg"
+        },
+        {
+            title: "Active Roof",
+            image: "/images/aisle-containment/dataracks-active-roof.jpg"
+        },
+        {
+            title: "Shrink Roof",
+            image: "/images/aisle-containment/dataracks-shrink-roof.jpg"
+        }
+    ]
     return (
         <>
             <Hero title="Aisle Containment" image='/images/dataracks-aisle-header.jpg' />
+            <section className='max-w-7xl mx-auto px-4 py-20'>
+                <div className='grid md:grid-cols-2 gap-10'>
+                    <div><p className='md:text-5xl text-3xl font-light'>Maximize uptime & efficiency with custom Aisle Containment solutions</p></div>
+                    <div className='text-[#595853]'>
+                        <p>Data centres face growing pressure to reduce energy consumption without  compromising uptime. At Dataracks, we specialise in bespoke hot and cold aisle containment systems engineered to improve thermal efficiency,  enhance cooling performance, and deliver measurable cost savings.
+                            <br /> <br />
+                            Our custom-built solutions are designed to:
+                            <br /> <br />
+                            <ul className='list-disc list-inside'>
+                                <li>Reduce energy usage and cooling costs.</li>
+                                <li>Eliminate hot spots and optimise airflow.</li>
+                                <li>Improve Power Usage Effectiveness (PUE)</li>
+                                <li>Support your sustainability goals with a lower carbon footprint</li>
+                            </ul>
+                            <br /> 
+                            Whether you’re upgrading an existing facility or building from the ground up,  our containment systems can be tailored to meet your specific layout and cooling requirements. We also offer a full survey and installation  service to ensure maximum performance.
+                            <br /> <br />
+                            You can find out more about both hot aisle and cold aisle containment below. If you’re focused on one over the other, dedicated  pages with in-depth information are available. Have questions or need a  quote? Contact us today — we’re here to help.</p>
+                    </div>
+                </div>
+
+                <div className='pt-20'>
+                    <div className='grid md:grid-cols-2 gap-8'>
+                        {data.map((item, index) => (
+                            <div key={index} className='bg-[#EAF4FF]'>
+                                <img src={item.image} alt={item.title} className='w-full object-cover' />
+                                <div className='p-6'>
+                                    <h3 className='text-xl'>{item.title}</h3>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+
+
         </>
     );
 };
--- app/routes/products/server-cabinets.tsx
+++ app/routes/products/server-cabinets.tsx
@@ -11,9 +11,60 @@ export function meta({ }: Route.MetaArgs) {
 };
 
 const ServerCabinets = () => {
+    const serverData = [
+        {
+            title: 'Cabinets & Enclosures',
+            image: '/images/server-cabinets/dataracks-cabinets-&-enclosures.jpg'
+        },
+        {
+            title: 'Split & Colocation Cabinets',
+            image: '/images/server-cabinets/dataracks-split-&-colocation-cabinets.jpg'
+        },
+        {
+            title: 'Specialist & Custom Made Cabinets',
+            image: '/images/server-cabinets/dataracks-specialist-&-custom-made-cabinets.jpg'
+        },
+        {
+            title: 'Patching Rack',
+            image: '/images/server-cabinets/dataracks-patching-rack.jpg'
+        },
+        {
+            title: 'Chimney Cabinets',
+            image: '/images/server-cabinets/dataracks-chimney-cabinets.jpg'
+        },
+        {
+            title: 'Cabinet Extensions',
+            image: '/images/server-cabinets/dataracks-cabinet-extensions.jpg'
+        },
+        {
+            title: '505 Cabinets',
+            image: '/images/server-cabinets/dataracks-505-cabinets.jpg'
+        },
+        {
+            title: 'ETSI Cabinets',
+            image: '/images/server-cabinets/dataracks-ETSI-cabinet.jpg'
+        },
+        {
+            title: '606 Cabinets',
+            image: '/images/server-cabinets/dataracks-606-cabinets.jpg'
+        }   
+    ]
+
     return (
         <>
             <Hero title="Server Cabinets" image='/images/dataracks-server-cabinets-header.jpg' />
+            <section className='max-w-7xl mx-auto px-4 py-20'>
+                <div className='grid md:grid-cols-3 gap-6'>
+                    {serverData.map((item, index) => (
+                        <div key={index}>
+                            <img src={item.image} alt={item.title} className='w-full object-cover' />
+                            <div className='p-4'>
+                                <h2 className='text-lg font-semibold mb-2'>{item.title}</h2>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
         </>
     );
 };
